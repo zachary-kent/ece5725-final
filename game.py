84ca437556cd57eb7f5d06be2f4e2327a762adaf
Zak Kent <zak33@cornell.edu>
2023-05-14 21:09:24 -0400

    Refactored code into for-loop

--- game.py
+++ game.py
@@ -72,14 +72,8 @@ try:
             leaderboard.draw(screen, clock, width, height, text_font, white)
             topscores_clicked = not leaderboard.handle_events()
         else:
-            screen.blit(text_buttons_dict["Quit"]
-                        [0], text_buttons_dict["Quit"][1])
-            screen.blit(text_buttons_dict["Score"][0],
-                        text_buttons_dict["Score"][1])
-            screen.blit(text_buttons_dict["New Game"]
-                        [0], text_buttons_dict["New Game"][1])
-            screen.blit(text_buttons_dict["Top Scores"]
-                        [0], text_buttons_dict["Top Scores"][1])
+            for text, rect in text_buttons_dict.values():
+                screen.blit(text, rect)
             game_board.draw(screen, width, height)
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
