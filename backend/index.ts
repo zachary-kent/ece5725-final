d2c24941a99197f1dab0a7735aff5adaf662261e
Zak Kent <zak33@cornell.edu>
2023-05-17 17:07:14 -0400

    Commented backend code

--- backend/index.ts
+++ backend/index.ts
@@ -4,76 +4,109 @@ import { IncorrectPassword, UserNotFound, createUser, highScore, highScores, log
 import { serve, setup } from 'swagger-ui-express'
 import docs from './docs.json'
 
+/** The express application */
 const app = express();
 
+// Parse request bodies as JSON
 app.use(express.json())
 
+// Setup the documentation endpoint
 app.use('/docs', serve, setup(docs))
 
+
+/**
+ * Middleware for authenticating an access token and saving it in the
+ * locals of a request.
+ * 
+ * @param req The HTTP request
+ * @param res The HTTP response
+ * @param next The next middleware in the application cycle
+ * @returns the reponse
+ */
 const validateToken = async (req, res, next) => {
   const token = req.headers['x-access-token'];
   if (token == null) {
     return res.status(401).send("Access token required");
   }
   try {
+    // Get username token was signed with
     const username = verify(token, process.env.SECRET);
+    // Save username in locals of request
     res.locals.username = username;
   } catch {
+    // Token malformed
     return res.status(403).send("Invalid access token");
   }
   return next();
 }
 
+// Endpoint for creating an account
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   if (username == null || password == null) {
+    // Malformed request body
     return res.status(400).send("Username and password required");
   }
   try {
+    // Create user in database
     await createUser({ username, password });
     return res.status(200).send("User created");
   } catch {
+    // Duplicate user
     return res.status(400).send("User already exists");
   }
 });
 
+// Endpoint for logging in an obtaining access token
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   if (username == null || password == null) {
+    // Malformed request body
     return res.status(400).send('Username and password required');
   }
   try {
+    // Get access token from logging in
     const token = await login({ username, password });
+    // Send it back to user
     return res.status(200).send({ token });
   } catch (e) {
     if (e instanceof IncorrectPassword) {
+      // User provided incorrect password
       return res.status(400).send('Incorrect password');
     }
     if (e instanceof UserNotFound) {
+      // User does not exist
       return res.status(400).send('User not found');
     }
     return res.status(400).send('Invalid credentials');
   }
 })
 
+// Endpoint for updating a user's high score
 app.post('/high-score', validateToken, async (req, res) => {
   const { score } = req.body;
   if (score == null) {
+    // Malformed request body
     return res.status(400).send("Score required");
   }
   try {
+    // Set high score in database
     await setHighScore(res.locals.username, score);
     return res.status(200).send("High score updated");
   } catch {
+    // Only occurs when user not found in database
     return res.status(400).send("User not found");
   }
 });
 
+// Endpoint for retrieving a user's high score
 app.get('/high-score', validateToken, async (req, res) => {
   try {
+    // Get score of user associated with access token
     const score = await highScore(res.locals.username);
     return res.status(200).send({ score })
   } catch {
+    // User not found
     return res.status(400).send("User not found");
   }
 });
@@ -82,14 +115,18 @@ app.get('/high-score/rankings', async (req, res) => {
   try {
     const { limit } = req.query;
     if (limit == null) {
+      // Send back all high scores
       const scores = await highScores();
       return res.status(200).send({ scores })
     }
     if (typeof limit !== 'string') {
+      // limit has incorrect type
       return res.status(400).send('Limit has incorrect type');
     }
+    // Parse string given representing limit
     const max = parseInt(limit, 10);
     if (Number.isNaN(max)) {
+      // limit string does not represent number
       return res.status(400).send('Cannot parse limit');
     }
     const scores = await highScores(max);
@@ -99,6 +136,7 @@ app.get('/high-score/rankings', async (req, res) => {
   }
 });
 
+/** The port the app will accept requests from */
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
--- backend/user.ts
+++ backend/user.ts
@@ -5,53 +5,85 @@ import { getFirestore } from 'firebase-admin/firestore'
 import { hash, compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+/** The secret key used to access our Firestore */
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT ?? readFileSync('./serviceAccount.json').toString());
 
 const credential = cert(serviceAccount);
 
+/** The Firebase app itself */
 const app = initializeApp({ credential })
 
+/** The database associated with our Firebase app */
 const firestore = getFirestore(app);
 
+/** Represents a user in Firestore */
 type FirestoreUser = {
+  /** The hashed password of this user */
   password: string
+  /** The high score of this user */
   score: number
 }
 
+/** The collection of all 2048 userss */
 const users = firestore.collection('users')
 
+/** A user, as viewed by the API */
 type User = {
+  /** The username of this user */
   username: string
+  /** This user's plaintext password */
   password: string
 };
 
-
+/**
+ * Create a new user in the database
+ * 
+ * @param user the user to create
+ */
 export const createUser = async ({ username, password }: User) => {
   const userDocRef = users.doc(username)
   const userDoc = await userDocRef.get();
   if (userDoc.exists) {
+    // Do not overwrite existing user
     throw new Error('User already exists');
   }
+  // Create new user
   await userDocRef.set({
     password: await hash(password, 8),
     score: 0,
   })
 };
 
+/**
+ * Get the high score of a user
+ * 
+ * @param username the user's name
+ * @returns The high score of user with username `username`
+ */
 export const highScore = async (username: string): Promise<number> => {
   const docRef = users.doc(username);
   const userDoc = await docRef.get();
   if (!userDoc.exists) {
-    throw new Error('User not found');
+    // User does not exist in database
+    throw new UserNotFound();
   }
   return userDoc.data().score;
 }
 
+/**
+ * Get an array of ranked high scores
+ * 
+ * @param max The maximum number of scores to return
+ * @returns At most `max` high scores
+ */
 export const highScores = async (max?: number): Promise<Array<{ username: string; score: number }>> => {
+  // User all users in descending order of high score
   let q = users.orderBy('score', 'desc');
   if (max != null) {
+    // If maximum number given, limit query
     q = q.limit(max);
   }
+  // Execute query
   const { docs } = await q.get();
   return docs.map(doc => {
     const { score } = doc.data() as FirestoreUser;
@@ -59,14 +91,24 @@ export const highScores = async (max?: number): Promise<Array<{ username: string
   });
 }
 
+/**
+ * Set a user's high score in the database
+ * 
+ * @param username the username of the user
+ * @param score the new high score
+ * @returns a Promise that resolves when the high score is updated in Firestore
+ */
 export const setHighScore = async (username: string, score: number) => {
   const userDocRef = users.doc(username);
   const userDoc = await userDocRef.get();
-  if (!userDoc.exists) throw new Error('User not found');
+  // User does not exist in database
+  if (!userDoc.exists) throw new UserNotFound();
+  // Do nothing if new score is less than current high score
   if (userDoc.data().score >= score) return;
   await userDocRef.update({ score });
 }
 
+/** Represents an error indicating that the requested user does not exist */
 export class UserNotFound extends Error {
   constructor(msg?: string) {
     super(msg);
@@ -74,6 +116,7 @@ export class UserNotFound extends Error {
   }
 }
 
+/** Represents an error indicating that an incorrect password was entered */
 export class IncorrectPassword extends Error {
   constructor(msg?: string) {
     super(msg);
@@ -81,6 +124,12 @@ export class IncorrectPassword extends Error {
   }
 }
 
+/**
+ * Login to receive an access token
+ * 
+ * @param user the provided credentials
+ * @returns An access token uniquely identiftying the user
+ */
 export const login = async ({ username, password }: User): Promise<string> => {
   const userDocRef = users.doc(username);
   const userDoc = await userDocRef.get();
